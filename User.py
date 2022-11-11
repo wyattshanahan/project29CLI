488ac4a4b3fa704c08f7c349372fdbd8e84e2666
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2022-11-10 18:03:14 -0600

    added some menu option stuff, seperated User class into separate file and built constructor

---
+++ User.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, userID, fname, lname, street, city, state, userZip, username, password, email, telephone, cardNum, cvv, cardName,cardDate, orderNum):
+        self.userID = userID
+        self.fname = fname
+        self.lname = lname
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zip = userZip
+        self.username = username
+        self.password = password
+        self.email = email
+        self.telephone = telephone
+        self.cardNum = cardNum
+        self.cvv = cvv
+        self.cardName = cardName
+        self.cardDate = cardDate
+        self.orderNum = orderNum
--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import mysql.connector
 import sys
+from User import User
 # Attempts a connection to the database server, displays a message if a connection is successful
 try:
     mydb = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "",
-        database = "project29"
+        host="localhost",
+        user="root",
+        password="",
+        database="project29"
     )
     print("Successfully connected to the database")
-# If connecting to the database server, an error message is displayed and the process ends
+# If connecting to the database server fails, an error message is displayed and the process ends
 except:
     print("Failed to connect to the database")
     sys.exit()
-print("code executed and connected successfully")
-
-# Class definition for the user class. This class still needs to be written
-class User:
-    def __init__(self, username):
-        self.username = username
 
 # class definitions and functions go above here
+def makeUser():
+    fname = input("Enter your first name: ")
+    lname = input("Enter your last name: ")
+    email = input("Enter your email: ")
+    telephone = input("Enter your telephone number: ")
+    #SQL to check for it already existing
+    username = input("Enter your desired username: ")
+    password = input("Enter your desired password: ")
+    #SQL to check for it already existing
+    street = input("Enter your street address: ")
+    city = input("Enter your city: ")
+    state = input("Enter your state: ")
+    userZIP = input("Enter your zip/postal code: ")
+    cardNum = input("Enter your credit card number: ")
+    cardDate = input("Enter your card's expiration date: ")
+    cardName = input("Enter the name on your card: ")
+    cardCVV = input("Enter the security code on your card: ")
+    orderNum = 0
+    userID = "some sql command to get number of rows in a table +1"
+    newUser = User(userID, fname, lname, street, city, state, userZIP, username, password, email, telephone, cardNum, cardCVV, cardName,cardDate, orderNum)
 
+    #function call to add to database
 # Main menu/ driver code (write a better comment than this for the final version)
 #each option below should launch the respective menu
 killprogram = False
@@ -57,16 +73,16 @@ while (killprogram == False):
                 print("Invalid option, please try again")
     elif (selection == "4"):
         print("this will eventually show order history")
-    #fix formatting of exit
+    # fix formatting of exit
     # Else if 5 is entered, then launch the exit menu
     elif (selection == "5"):
         while (1):
-            print("Do you wish to log out?")
+            print("\nDo you wish to log out?")
             print("1. Log Out")
             print("2. Go Back")
             selection = input("Input a number to select a menu option: ")
             if (selection == "1"):
-                print(" Logging you out")
+                print("\nYou will now be logged out")
                 killprogram = True
                 break
             elif (selection == "2"):
