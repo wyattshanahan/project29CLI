4d08320aabb2d039eff648575c32757f77323a05
bladn <bladner0717@gmail.com>
2022-11-15 11:07:28 -0600

    video game class

---
+++ VideoGame.py
@@ -0,0 +1,14 @@
+class VideoGame:
+    def __init__(self, title, developer, release_year, console, gameID, in_stock):
+        self.title = title
+        self.developer = developer
+        self.release_year = release_year
+        self.console = console
+        self.gameID = gameID
+        self.in_stock = in_stock
+
+    def viewInfo(gameID):
+        return 0
+
+    def setStock(gameID, newStock):
+        return 0
