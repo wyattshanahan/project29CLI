59794019b69b7357a62b22152a29010e209805ad
bladn <bladner0717@gmail.com>
2022-11-17 11:41:11 -0600

    Added while loops to the menus in main

--- main.py
+++ main.py
@@ -83,55 +83,58 @@ def login():
 killprogram = False
 while (killprogram == False):
     print("\nMain Menu:")
-    print("1. Shop Inventory")
-    print("2. View and Edit Cart")
-    print("3. View and Edit User Information")
-    print("4. View Order History")
-    print("5. Log Out")
-    selection = input("Input a number to select a menu option: ")
-    if (selection == "1"):
-        print("this will eventually show inventory")
-        makeNewUser()
-    elif (selection == "2"):
-        print("this will eventually show the cart menu")
-    elif (selection == "3"):
-        while (1):
-            print("User Information: ")
-            print("1. View User Information")
-            print("2. Edit User Information")
-            print("3. Delete User Account")
-            print("4. Go Back")
-            selection = input("Input a number to select a menu option: ")
-            if (selection == "1"):
-                print("display User Information here with a function")
-            elif (selection == "2"):
-                print("edit user info menu, after displaying info")
-            elif (selection == "3"):
-                print("Delete account menu, used to confirm deletion then function to delete it")
-            elif (selection == "4"):
-                break
-            else:
-                print("Invalid option, please try again")
-    elif (selection == "4"):
-        print("this will eventually show order history")
-    # fix formatting of exit
-    # Else if 5 is entered, then launch the exit menu
-    elif (selection == "5"):
-        while (1):
-            print("\nDo you wish to log out?")
-            print("1. Log Out")
-            print("2. Go Back")
-            selection = input("Input a number to select a menu option: ")
-            if selection == "1":
-                print("\nYou will now be logged out")
-                killprogram = True
-                break
-            elif (selection == "2"):
-                break
-            else:
-                print("Invalid option, please try again")
-    else:
-        print("Invalid option, please try again")
+    while selection not in ['1', '2', '3', '4', '5']:
+        print("1. Shop Inventory")
+        print("2. View and Edit Cart")
+        print("3. View and Edit User Information")
+        print("4. View Order History")
+        print("5. Log Out")
+        selection = input("Input a number to select a menu option: ")
+        if (selection == "1"):
+            print("this will eventually show inventory")
+            makeNewUser()
+        elif (selection == "2"):
+            print("this will eventually show the cart menu")
+        elif (selection == "3"):
+            # userinfo_selection variable so that the while loop works
+            userinfo_selection = ''
+            while userinfo_selection not in ['1', '2', '3', '4']:
+                print("User Information: ")
+                print("1. View User Information")
+                print("2. Edit User Information")
+                print("3. Delete User Account")
+                print("4. Go Back")
+                selection = input("Input a number to select a menu option: ")
+                if (userinfo_selection == "1"):
+                    print("display User Information here with a function")
+                elif (userinfo_selection == "2"):
+                    print("edit user info menu, after displaying info")
+                elif (userinfo_selection == "3"):
+                    print("Delete account menu, used to confirm deletion then function to delete it")
+                elif (userinfo_selection == "4"):
+                    break
+                else:
+                    print("Invalid option, please try again\n")
+        elif (selection == "4"):
+            print("this will eventually show order history")
+        # fix formatting of exit
+        # Else if 5 is entered, then launch the exit menu
+        elif (selection == "5"):
+            while selection not in ['1', '2']:
+                print("\nDo you wish to log out?")
+                print("1. Log Out")
+                print("2. Go Back")
+                selection = input("Input a number to select a menu option: ")
+                if selection == "1":
+                    print("\nYou will now be logged out")
+                    killprogram = True
+                    break
+                elif (selection == "2"):
+                    break
+                else:
+                    print("Invalid option, please try again")
+        else:
+            print("Invalid option, please try again\n")
 
 
 #TO-DO:
