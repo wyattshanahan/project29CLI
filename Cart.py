a988f16565259ca39f7d8060b119e6c3986a61d9
mag838 <117929596+mag838@users.noreply.github.com>
2022-11-17 11:55:04 -0600

    Add files via upload

---
+++ Cart.py
@@ -0,0 +1,20 @@
+class Cart:
+    def __init__(self, itemID, quantity):
+        self.itemID = itemID
+        self.quantity = quantity
+
+    def viewCart(self):
+        print("Title: {self.title}" + self.quantity)
+
+
+    def addCart(self, cursor, mydb):
+        cursor = cursor
+        userInput = input("What game would you would you like to add to your cart? ")
+        query =
+
+
+    def remaveCart(self):
+
+
+    def checkout(self):
+
