f95a62a38bca75087d3c3238d3dd8c317b677741
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2022-11-24 16:32:53 -0600

    Adjusted and updated the users SQL database

--- users.sql
+++ users.sql
@@ -51,9 +51,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userID`, `fname`, `lname`, `street`, `city`, `state`, `userZip`, `username`, `password`, `email`, `telephone`, `cardNum`, `cvv`, `cardName`, `cardDate`, `orderNum`) VALUES
-(0, 'Jack', 'Pistachio', '12 MS 12-W', 'Ackerman', 'MS', '39738', 'Jpistachio', 'bigCooli0', 'jackpistachio292@gmail.com', '662-361-0428', '1239203847289172', 122, 'Jack Pistachio', '11/2012', 0),
-(0, 'Neil', 'Yakapov', '237 91st Street', 'Edmonton', 'AB', 'T6E 2Z7', 'nyakapov', 'pAsSwOrD!', 'nyakapov@nhl.com', '627-232-1242', '1212646423234232', 134, 'Neil Yakapov', '12/2022', 0);
+(1, 'Luke', 'Greene', '175 Presidents Circle', 'Mississippi State', 'MS', '39762', 'lgreene', 'superSecurePass!', 'lgreene1969@hotmail.com', '662-325-0428', '3829102938294736', 12, 'Luke Greene', '12/2025', 0),
+(2, 'Neil', 'Yakapov', '237 91st Street', 'Edmonton', 'AB', 'T6E 2Z7', 'nyakapov', 'pAsSwOrD!', 'nyakapov@nhl.com', '627-232-1242', '1212646423234232', 134, 'Neil Yakapov', '12/2022', 0),
+(3, 'Jack', 'Pistachio', '12 MS 12-W', 'Ackerman', 'MS', '39738', 'Jpistachio', 'bigCooli0', 'jackpistachio292@gmail.com', '662-361-0428', '1239203847289172', 122, 'Jack Pistachio', '11/2012', 0);
+(4, 'David', 'Babbitt', '319 N Beaver St', 'Flagstaff', 'AZ', '86001', 'babbittybabbitt', 'superSecretCode19', 'dbabbitt@gmail.com', '928-774-0428', '1239203847238422', 672, 'David Babbitt', '01/2042', 0);
+
 COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
