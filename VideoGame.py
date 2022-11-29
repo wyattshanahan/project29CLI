251ed64baf2113a7d0b6ab56a22addc3a83b1d66
bladn <bladner0717@gmail.com>
2022-11-28 19:14:28 -0600

    Added price attribute to VideoGame class

--- VideoGame.py
+++ VideoGame.py
@@ -1,18 +1,20 @@
 class VideoGame:
-    def __init__(self, title, developer, release_year, console, gameID, in_stock):
+    def __init__(self, title, developer, release_year, console, gameID, in_stock, price):
         self.title = title
         self.developer = developer
         self.release_year = release_year
         self.console = console
         self.gameID = gameID
         self.in_stock = in_stock
+        self.price = price
 
     def viewInfo(self):
         print(f"Title: {self.title}\n"
               f"Developer: {self.developer}\n"
               f"Console: {self.console}\n"
               f"Game ID: {self.gameID}\n"
-              f"Number of copies in stock: {self.in_stock}\n")
+              f"Number of copies in stock: {self.in_stock}\n"
+              f"Price: {self.price}\n")
 
     def setStock(self, new_stock):
         self.in_stock = new_stock
