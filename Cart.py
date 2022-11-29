11e7a0539d780f7fad60ff8e7efb4e8ffc042012
mag838 <117929596+mag838@users.noreply.github.com>
2022-11-29 16:24:45 -0600

    Add files via upload

--- Cart.py
+++ Cart.py
@@ -1,20 +1,78 @@
 class Cart:
-    def __init__(self, itemID, quantity):
-        self.itemID = itemID
-        self.quantity = quantity
+    def __init__(self, Title, GameID, Quantity):
+        self.Title = Title
+        self.GameID = GameID
+        self.Quantity = Quantity
 
+# View items in your cart
     def viewCart(self):
-        print("Title: {self.title}" + self.quantity)
+        print("Cart:\n")
+        print("Title: " + self.Title + " --- " + self.GameID)
+        print("Quantity : " + self.Quantity)
 
-
+# Adds game to your cart SQL table
     def addCart(self, cursor, mydb):
         cursor = cursor
-        userInput = input("What game would you would you like to add to your cart? ")
-        query =
+        userInput = input("Enter the gameID of the game you would like to buy: ")
+        query = "SELECT GameID FROM inventory WHERE GameID = %s"
+        cursor.execute(query, userInput)
+        numID = cursor.fetchone()
 
+        query = "SELECT Title FROM inventory WHERE GameID = %s"
+        cursor.execute(query, numID)
+        title = cursor.fetchone()
 
-    def remaveCart(self):
+        query = "SELECT Quantity FROM inventory WHERE GameID = %s"
+        cursor.execute(query, numID)
+        stock = cursor.fetchone()
+        stock = stock - 1
+        query = "UPDATE inventory SET Quantity = s% WHERE GameID = %s"
+        cursor.execute(query, stock, numID)
 
+        cart = (title, 1, numID)
+        query = "INSERT INTO cart (Title, Quantity, GameID) VALUES (%s, %s, %s)"
+        cursor.execute(query, cart)
 
-    def checkout(self):
+        mydb.commit()
+
+# Removes game from the cart SQL table
+    def remaveCart(self, cursor, mydb):
+        cursor = cursor
+        userInput = input("Enter the gameID of the game you would like to remove from your cart: ")
+        query = "SELECT GameID FROM inventory WHERE GameID = %s"
+        cursor.execute(query, userInput)
+        numID = cursor.fetchone()
+
+        query = "DELETE FROM cart WHERE GameID = %s"
+        cursor.execute(query, numID)
+
+        query = "SELECT Quantity FROM inventory WHERE GameID = %s"
+        cursor.execute(query, numID)
+        stock = cursor.fetchone()
+        stock = stock + 1
+        query = "UPDATE inventory SET Quantity = s% WHERE GameID = %s"
+        cursor.execute(query, stock, numID)
+
+        mydb.commit()
+
+# Clears cart database and adds everything to order history
+    def checkout(self, cursor, mydb):
+        cursor = cursor
+        query = "SELECT GameID FROM cart"
+        cursor.exectue(query)
+        numID = cursor.fetchall()
+
+        query = "SELECT Quantity FROM cart"
+        cursor.exectue(query)
+        stock = cursor.fetchall()
+
+        query = "SELECT Title FROM cart"
+        cursor.exectue(query)
+        title = cursor.fetchall()
+
+        history = (title, stock, numID)
+        query = "INSERT INTO OrderHistory (Title, Quantity, GameID) VALUES (%s, %s, %s)"
+        cursor.execute(query, history)
+
+        mydb.commit()
 
