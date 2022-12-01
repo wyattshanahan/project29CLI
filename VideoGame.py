a98c66690e2127fcebb0e31f85e37b8597c7496a
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2022-11-30 19:38:26 -0600

    documentation and comments

--- VideoGame.py
+++ VideoGame.py
@@ -1,4 +1,5 @@
 class VideoGame:
+    # VideoGame class constructor
     def __init__(self, title, developer, release_year, console, gameID, price, in_stock):
         self.title = title
         self.developer = developer
@@ -7,7 +8,7 @@ class VideoGame:
         self.gameID = gameID
         self.in_stock = in_stock
         self.price = price
-
+    # function to display video game attributes
     def viewInfo(self):
         print(f"Title: {self.title}\n"
               f"Developer: {self.developer}\n"
@@ -15,6 +16,6 @@ class VideoGame:
               f"Game ID: {self.gameID}\n"
               f"Number of copies in stock: {self.in_stock}\n"
               f"Price: {self.price}\n")
-
+    # function to set stock
     def setStock(self, new_stock):
         self.in_stock = new_stock
